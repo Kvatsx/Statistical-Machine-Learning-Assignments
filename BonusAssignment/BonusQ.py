55481b88230eeb178ff8150e26055046f240c732
Kaustav Vats <kaustav16048@iiitd.ac.in>
2019-04-13 14:27:52 +0530

    Bovw Not working

--- BonusAssignment/BonusQ.py
+++ BonusAssignment/BonusQ.py
@@ -71,7 +71,7 @@ def PreProcessing(x_data):
 X_Data = PreProcessing(X_Data)
 
 # --------------------------------------------------------------------------
-X_Train, NX_Test, Y_Train, NY_Test = train_test_split(X_Data, Y_Data, test_size=0.3, random_state=42)
+# X_Train, NX_Test, Y_Train, NY_Test = train_test_split(X_Data, Y_Data, test_size=0.3, random_state=42)
 # count = DataPerClass(X_Train, Y_Train)
 
 # Save & Load Data --------------------------------------------------------
@@ -92,23 +92,25 @@ print("Data Saved!")
 
 # Feature Extraction ---------------------------------------------
 
-def ExtractFeatures(x_train, y_train):
+def SiftFeatures(x_train, y_train):
     sift = cv2.xfeatures2d.SIFT_create()
     features = []
     nx_train = []
     ny_train = []
     for i in range(x_train.shape[0]):
-        _, des = sift.detectAndCompute(x_train[i], None)
+        image = np.reshape(x_train[i], (64, 64, 3))
+        _, des = sift.detectAndCompute(image, None)
         if des is None:
             continue
-        features.extend(des)
+        for d in des:
+            features.append(d)
         nx_train.append(x_train[i])
         ny_train.append(y_train[i])
 
     features = np.asarray(features)
     nx_train = np.asarray(nx_train)
     ny_train = np.asarray(ny_train)
-    print("features.shape", features.shape)
+    print("Features.shape", features.shape)
 
     return nx_train, ny_train, features 
 
@@ -146,7 +148,7 @@ def getHOG(x_data):
         img = np.reshape(x_data[i], (64, 64, 3))
         fd, hog_image = hog(img, orientations=4, pixels_per_cell=(8, 8), cells_per_block=(1, 1), visualize=True, multichannel=True, block_norm="L2-Hys")
         hds.append(fd)
-        cv2.imwrite("./BonusData")
+        cv2.imwrite("./BonusData/Visualize_Train/img_"+str(i)+".png", hog_image)
     hds = np.asarray(hds)
     print("Hog.shape", hds.shape)
     return hds
@@ -174,8 +176,8 @@ def getHOG(x_data):
 
 # X_Train = getHisto(X_Train)
 # NX_Test = getHisto(NX_Test)
-X_Train = getHOG(X_Train)
-NX_Test = getHOG(NX_Test)
+# X_Train = getHOG(X_Train)
+# NX_Test = getHOG(NX_Test)
 # Accuracy = clf.score(X_Train, Y_Train)
 # print("Accuracy:", Accuracy*100)
 # Accuracy = clf.score(X_Test, Y_Test)
@@ -187,49 +189,90 @@ NX_Test = getHOG(NX_Test)
 # Y_Train = Y_Data
 # X_Test = getHisto(X_Test)
 
+print("---------Sift Features-----------------------")
+# size = int(X_Train.shape[0] * 0.5)
+# print(X_Train.shape)
+# X_Train_1 = X_Train[:size]
+# X_Train_2 = X_Train[size:]
+
+# print(X_Train_1.shape)
+# print(X_Train_2.shape)
+# print("Done")
+
+# Y_Train_1 = Y_Train[:size]
+# Y_Train_2 = Y_Train[size:]
+# print(Y_Train_1.shape)
+# print(Y_Train_2.shape)
+
+size = int(X_Data.shape[0]*0.5)
+X_Data_1 = X_Data[:size]
+X_Data_2 = X_Data[size:]
+
+Y_Data_1 = Y_Data[:size]
+Y_Data_2 = Y_Data[size:]
+
+# X_Train_1, Y_Train_1, Features1 = SiftFeatures(X_Train_1, Y_Train_1)
+X_Train_1, Y_Train_1, Features1 = SiftFeatures(X_Data_1, Y_Data_1)
+
+
 
 print("------------Clutering Features-----------------")
 
-# kmeans = KMeans(n_clusters=500)
-# kmeans.fit(Features)
-# pickle.dump(kmeans, open("./BonusData/Kmean.sav", 'wb'))
+kmeans = KMeans(n_clusters=50)
+kmeans.fit(Features1)
+pickle.dump(kmeans, open("./BonusData/Kmean.sav", 'wb'))
 # kmeans = pickle.load(open("./BonusData/Kmean.sav", 'rb'))
 
 # kmeans = pickle.load(open("./BonusData/Kmean.sav", 'rb'))
 def Bovw(kmeans, x_data, y_data):
     sift = cv2.xfeatures2d.SIFT_create()
     Features = []
-    Y_Data = []
+    Y_data = []
     for i in range(x_data.shape[0]):
+        image = np.reshape(x_data[i], (64, 64, 3))
         histogram = np.zeros(len(kmeans.cluster_centers_))
-        _, des = sift.detectAndCompute(x_data[i], None)
-        if des is None:
-            continue
-        histogram[kmeans.predict(des)] += 1
+        kp, des = sift.detectAndCompute(image, None)
+        # if des is None:
+        #     continue
+        nkp = np.size(kp)
+        if not des is None:
+            for d in des:
+                idx = kmeans.predict(np.reshape(d, (1, d.shape[0])))
+                histogram[idx] += 1
         Features.append(histogram)
-        Y_Data.append(y_data[i])
+        Y_data.append(y_data[i])
+
     Features = np.asarray(Features)
-    Y_Data = np.asarray(y_data)
-    return Features, Y_Data
+    Y_data = np.asarray(Y_data)
+    return Features, Y_data
                              
-# NX_Train, NY_Train = Bovw(kmeans, X_Train, Y_Train)
-# NX_Test, NY_Test = Bovw(kmeans, X_Test, Y_Test)
+# print("Vocab Test: ", X_Train_2.shape, Y_Train_2.shape)
+# X_Train, Y_Train = Bovw(kmeans, X_Train_2, Y_Train_2)
+# NX_Test, NY_Test = Bovw(kmeans, NX_Test, NY_Test)
+
+X_Train, Y_Train = Bovw(kmeans, X_Data_2, Y_Data_2)
+
+X_Test, names = ReadTestData()
+X_Test = PreProcessing(X_Test)
+NX_Test, names = Bovw(kmeans, X_Test, names)
 
 print("------------Training Classifier-----------------")
 
-# print(X_Train.shape)
-# clf = svm.SVC(gamma='auto')
-# clf.fit(X_Train, Y_Train)
+print(X_Train.shape, Y_Train.shape)
+clf = svm.SVC(gamma='auto')
+clf.fit(X_Train, Y_Train)
 # pickle.dump(clf, open("./BonusData/Svm.sav", 'wb'))
 # clf = pickle.load(open("./BonusData/Svm.sav", 'rb'))
 
 print("-------------Training Complete------------------")
 
 # NX_Test, NY_Test, _ = ExtractFeatures(X_Test, Y_Test)
-# Accuracy = clf.score(X_Train, Y_Train)
-# print("Accuracy[Train]:", Accuracy*100)
+Accuracy = clf.score(X_Train, Y_Train)
+print("Accuracy[Train]:", Accuracy*100)
 # Accuracy = clf.score(NX_Test, NY_Test)
 # print("Accuracy[Test]:", Accuracy*100)
+labels = clf.predict(NX_Test)
+WriteCsv("2016048_kaustav_submission1.csv", names, labels)
 
 
 clf = RandomForestClassifier(n_estimators=1000, random_state=0)
@@ -238,15 +281,28 @@ Accuracy = clf.score(X_Train, Y_Train)
 print("[RFC]Accuracy[Train]:", Accuracy*100)
 # labels = clf.predict(X_Test)
 
+labels = clf.predict(NX_Test)
+WriteCsv("2016048_kaustav_submission2.csv", names, labels)
+
 # np.save("./BonusData/labels.npy", labels)
 # labels = np.load("./BonusData/labels.npy")
 # print(names)
 # WriteCsv("2016048_kaustav_submission.csv", names, labels)
-Accuracy = clf.score(NX_Test, NY_Test)
-print("[RFC]Accuracy[Test]:", Accuracy*100)
+# Accuracy = clf.score(NX_Test, NY_Test)
+# print("[RFC]Accuracy[Test]:", Accuracy*100)
 
 
 # Accuracy = clf.score(X_Train, Y_Train)
 # print("[DTC]Accuracy[Train]:", Accuracy*100)
 # Accuracy = clf.score(X_Test, Y_Test)
 # print("[DTC]Accuracy[Test]:", Accuracy*100)
+
+# from sklearn.neighbors import KNeighborsClassifier
+# clf = KNeighborsClassifier(n_neighbors=50)
+# clf.fit(X_Train, Y_Train)
+
+# PredictedLabels = clf.predict(NX_Test)
+# Accuracy = clf.score(X_Train, Y_Train)
+# print("[KNN]Accuracy[Train]:", Accuracy*100)
+# Accuracy = clf.score(NX_Test, NY_Test)
+# print("[KNN]Accuracy[Test]:", Accuracy*100)
