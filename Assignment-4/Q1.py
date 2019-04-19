e43ee308666294acb6ec5f97f198aafdde0ce693
Kaustav Vats <kaustav16048@iiitd.ac.in>
2019-04-19 22:47:39 +0530

    A4 Q1 Init

--- Assignment-4/Q1.py
+++ Assignment-4/Q1.py
@@ -1,4 +1,12 @@
 # Kaustav Vats (2016048)
+# Ref:- [1] https://zhenye-na.github.io/2018/09/09/build-neural-network-with-mnist-from-scratch.html
+# Ref:- [2] https://jonathanweisberg.org/post/A%20Neural%20Network%20from%20Scratch%20-%20Part%201/
+# Ref:- [3] https://enlight.nyc/projects/neural-network/
+# Ref:- [4] http://www.wildml.com/2015/09/implementing-a-neural-network-from-scratch/
+# Ref:- [5] https://github.com/gary30404/neural-network-from-scratch-python/blob/master/network.py
+# Ref:- [6] https://github.com/vzhou842/neural-network-from-scratch/blob/master/network.py
+# Ref:- [7] https://github.com/dennybritz/nn-from-scratch/blob/master/nn-from-scratch.ipynb
+# Ref:- [8] https://towardsdatascience.com/how-to-build-your-own-neural-network-from-scratch-in-python-68998a08e4f6
 
 import numpy as np
 import cv2
@@ -8,53 +16,63 @@ from utils import ReadData
 # Neural Netwrok ---------------------------------
 class NeuralNetwork:
 
-    def __init__(self, ncx, nco, nch1=256, nch2=128, nch3=64, nch4=0, actFunc="relu", learning_rate=0.1, epochs=100, verbose=True):
-        self.ActFunc = actFunc
-        self.LearningRate = learning_rate
-        self.Epochs = epochs
-        self.W1 = np.random.rand(ncx, nch1)
-        self.B1 = np.random.rand(1, nch1)
-        self.W2 = np.random.rand(nch1, nch2)
-        self.B1 = np.random.rand(1, nch3)
-        self.W3 = np.random.rand(nch2, nch3)
-        self.B1 = np.random.rand(1, nch3)
-        self.Output = np.random.rand(nch3, nco)
-        if nch4 != 0:
-            self.W4 = np.random.rand(nch3, nch4)
-            self.B1 = np.random.rand(1, nch4)
-            self.Output = np.random.rand(nch4, nco)
-        if verbose:
-            print("Activation Function:\t{}".format(self.ActFunc))
-            print("Learning Rate:\t{}".format(self.LearningRate))
-            print("Epochs:\t{}".format(self.Epochs))
-            print("Input Layer:\t{}".format(ncx))
-            print("Layer 1:\t{}".format(self.W1.shape))
-            print("Layer 2:\t{}".format(self.W2.shape))
-            print("Layer 3:\t{}".format(self.W3.shape))
-            if nch4 != 0:
-                print("Layer 4:{}".format(self.W4.shape))
-            print("Output Layer:{}".format(self.Output.shape))
+    def __init__(self, input_size, nodes_hidden, verbose=True):
+        self.InputSize = input_size
+        self.LayersCount = len(nodes_hidden)
+        self.W = []
+        self.B = []
+        self.Initialize(nodes_hidden)
 
-    def Sigmoid(self, zx):
-        ret = 1/(1 + np.exp(-zx))
+        if verbose:
+            print("------ Neural Network Structure -------")
+            for i in range(self.LayersCount):
+                print("[W] Layer {} shape: {}".format(i, self.W[i].shape))         
+            for i in range(self.LayersCount):
+                print("[B] Layer {} shape: {}".format(i, self.B[i].shape))
+
+    def Initialize(self, node_hidden):
+        prev = self.InputSize
+        for i in range(self.LayersCount):
+            now = node_hidden[i]
+            self.W.append(np.random.randn(prev, now)/np.sqrt(now))
+            self.B.append(np.random.randn(1, now)/np.sqrt(now))
+            prev = now
+
+    def Sigmoid(self, z):
+        ret = 1/(1 + np.exp(-z))
         return ret
     
-    def Relu(self, zx):
-        if zx > 0:
-            return zx
+    def Relu(self, z):
+        if z > 0:
+            return z
         return 0
 
+    def Sigmoid_Derivative(self, z):
+        ret = z * (1 - z)
+        return ret
+    
+    def Softmax(self, z):
+        Exp = np.exp(z)
+        Total = np.sum(Exp, axis=1)
+        return Exp / Total
+
     def FeedForward(self, x_data, actFunc="relu"):
-        Z1 = np.dot(x_data, self.W1) + self.B1
-        self.A1 = self.Sigmoid(Z1)
-        Z2 = np.dot(self.A1, self.W2) + self.B1
-        self.A2 = self.Sigmoid(Z2)
-        Z3 = np.dot(self.A2, self.W3) + self.B1
-        self.A3 = self.Sigmoid(Z3)
+        self.A = []
+        Activation = x_data
+        for i in range(self.LayersCount):
+            z = np.dot(Activation, self.W[i]) + self.B[i]
+            A = self.Sigmoid(z)
+            self.A.append(A)
 
     '''Updating weights of all neurons after training'''
-    def BackPropogation(self):
+    def BackPropogation(self, y_data, output):
+        self.E = y_data - output
+        # self.
 
+    def saveModel(self):
+        for i in range(self.LayersCount):
+            np.save("./Data/W{}.npy".format(i), self.W[i])
+            np.save("./Data/B{}.npy".format(i), self.B[i])        
 
 
 
@@ -63,4 +81,4 @@ if __name__ == "__main__":
     X_Train, Y_Train, X_Test, Y_Test = ReadData(verbose=True)
     print("------------------------------------------------")
 
-    NN = NeuralNetwork(X_Train.shape[0], np.unique(Y_Train).shape[0], nch4=0)
+    NN = NeuralNetwork(X_Train.shape[0], [256, 128, 64])
